10a55deadbc341d107e5d3520651968d99a65a11
Justin Rhodes <justin.m.rhodes@gmail.com>
2025-12-21 21:18:46 -0600

    feat: enhance player resolver and service with detailed documentation

--- src/players/players.resolver.ts
+++ src/players/players.resolver.ts
@@ -3,10 +3,18 @@ import { Resolver, Query, Args, Mutation, ID } from '@nestjs/graphql';
 import { CreatePlayerInput } from './dtos/create-player.input';
 import { PlayersService } from './players.service';
 
+/**
+ * Resolver for the Player entity
+ * @description This resolver handles all the GraphQL queries and mutations for the Player entity
+ */
 @Resolver(() => Player)
 export class PlayersResolver {
   constructor(private playersService: PlayersService) {}
 
+  /**
+   * Fetches all players from the database
+   * @returns
+   */
   @Query(() => [Player])
   async getAllPlayers(): Promise<Player[]> {
     try {
@@ -17,12 +25,22 @@ export class PlayersResolver {
     }
   }
 
+  /**
+   * Fetches a single player by id
+   * @param Args id
+   * @returns
+   */
   @Query(() => Player)
   async getPlayer(@Args('id', { type: () => ID }) id: string): Promise<Player> {
     const player = await this.playersService.getPlayerById(id);
     return player;
   }
 
+  /**
+   * Creates a new player in the database
+   * @param createPlayerData - The data for the new player
+   * @returns
+   */
   @Mutation(() => Player)
   async createPlayer(
     @Args('createPlayerData') createPlayerData: CreatePlayerInput,
--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -4,19 +4,42 @@ import { Player } from './schemas/player.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
+/**
+ * The service for managing players.
+ * @constructor
+ * @Injectableparam playerModel - The Mongoose model for the Player schema.
+ */
 @Injectable()
 export class PlayersService {
   constructor(@InjectModel(Player.name) private playerModel: Model<Player>) {}
 
+  /**
+   * Creates a new player in the database.
+   *
+   * @param createPlayerInput - The input data for creating a new player.
+   * @returns
+   */
   async createPlayer(createPlayerInput: CreatePlayerInput): Promise<Player> {
     const player = new this.playerModel(createPlayerInput);
     return player.save();
   }
 
+  /**
+   * Retrieves all players from the database.
+   *
+   * @returns
+   */
   async getAllPlayers(): Promise<Player[]> {
     return this.playerModel.find().exec();
   }
 
+  /**
+   * Retrieves a player by their ID.
+   *
+   * @param id - The ID of the player to retrieve.
+   * @throws Error if the player is not found.
+   * @returns
+   */
   async getPlayerById(id: string): Promise<Player> {
     const player = await this.playerModel.findById(id).exec();
     if (!player) {
