e599ca41f8b734177d276fe1b79a081919e64b28
Justin Rhodes <justin.m.rhodes@gmail.com>
2026-02-02 21:40:49 -0600

    feat(players): remove Mongoose decorators from Player schema

--- src/players/schemas/player.schema.ts
+++ src/players/schemas/player.schema.ts
@@ -1,30 +1,22 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Field, ID, ObjectType } from '@nestjs/graphql';
 import { IsOptional } from 'class-validator';
 
 @ObjectType() // GraphQL Object Type decorator
-@Schema({ timestamps: true }) // Mongoose Schema decorator
 export class Player {
   @Field(() => ID) // GraphQL Field for the 'id' (MongoDB's _id)
   id: string;
 
   @Field()
-  @Prop({ required: true })
   name: string;
 
   @Field()
   @IsOptional()
-  @Prop({ required: false, default: 0, nullable: true })
   pgaId: number;
 
   @Field()
-  @Prop()
   salary: number;
 
   @Field(() => String, { nullable: true })
   @IsOptional()
-  @Prop({ required: false, default: null, nullable: true })
   avatarUrl: string;
 }
-
-export const PlayerSchema = SchemaFactory.createForClass(Player);
